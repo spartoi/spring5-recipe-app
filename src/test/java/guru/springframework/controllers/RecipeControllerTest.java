9d67bebe3f992176a08b4116cd321683b04626b4
scotttiger <renegadeoffunk1@gmail.com>
2021-05-16 20:00:35 +0300

    further commits

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -36,7 +36,8 @@ public class RecipeControllerTest {
         MockitoAnnotations.initMocks(this);
 
         controller = new RecipeController(recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setControllerAdvice(new ControllerExceptionHandler()).build();
     }
 
     @Test
@@ -63,6 +64,15 @@ public class RecipeControllerTest {
                 .andExpect(view().name("404error"));
     }
 
+    @Test
+    public void testGetRecipeNumFormatException() throws Exception {
+
+        //when(recipeService.findById(anyLong())).thenThrow(NumberFormatException.class);
+
+        mockMvc.perform(get("/recipe/aaa/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
     @Test
     public void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
